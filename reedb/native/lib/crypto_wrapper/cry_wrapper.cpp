9c9698caf4eaa76262255053d5ce3827e5343efa
SpaceKookie <sabel.katharina@gmail.com>
2015-07-01 00:05:08 +0200

    Adding C++ sources for the wrapper around Crypto++

---
+++ reedb/native/lib/crypto_wrapper/cry_wrapper.cpp
@@ -0,0 +1,10 @@
+/*
+ * cry_wrapper.cpp
+ *
+ *  Created on: 30 Jun 2015
+ *      Author: spacekookie
+ */
+
+
+
+
---
+++ reedb/native/lib/crypto_wrapper/cry_wrapper.hpp
@@ -0,0 +1,23 @@
+/*
+ * cry_wrapper.hpp
+ *
+ *  Created on: 30 Jun 2015
+ *      Author: spacekookie
+ */
+
+#ifndef CRYPTO_WRAPPER_CRY_WRAPPER_HPP_
+#define CRYPTO_WRAPPER_CRY_WRAPPER_HPP_
+
+#include <stdlib.h>
+#include "cryptopp/modes.h"
+#include "cryptopp/aes.h"
+
+using namespace std;
+
+// ...
+
+//byte key[AES::DEFAULT_KEYLENGTH], iv[AES::BLOCKSIZE];
+//// initialize key and iv here
+//CFB_Mode<AES>::Encryption cfbEncryption(key, AES::DEFAULT_KEYLENGTH, iv);
+
+#endif /* CRYPTO_WRAPPER_CRY_WRAPPER_HPP_ */
