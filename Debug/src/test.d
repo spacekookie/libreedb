02b8f4a5419cb3dd3f15ff6d633a8f04897914ec
Katharina Sabel <sabel.katharina@gmail.com>
2015-08-22 12:17:27 +0200

    Adding GDSL copy to repo. This is temporary. Will probably use a submodule at some point. Huzza!

--- Debug/src/test.d
+++ Debug/src/test.d
@@ -1,5 +1,1 @@
-src/test.d: ../src/test.c ../src/core.h ../src/utils.h
-
-../src/core.h:
-
-../src/utils.h:
+src/test.d: ../src/test.c
--- src/test.c
+++ src/test.c
@@ -5,34 +5,37 @@
  *      Author: spacekookie
  */
 
-#include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
-
-#include "core.h"
-#include "utils.h"
+#include <gdsl.h>
 
 int main(void)
 {
-	ree_err_t rtn;
 
-	struct reedb_c *container;
-
-	rtn = rdb_set_passlength(16);
-	rtn = rdb_set_verbose(true);
-	rtn = rdb_set_path("/etc/reedb/");
-
-	rtn = reedb_init(&container);
-	if (rtn != BIG_SUCCESS)
-	{
-		fputs("An error occurred when initialising Reedb!\n", stderr);
-	}
-	else
-	{
-		printf("Reedb was initialised successfully!\n");
-	}
-
-	free(container);
-	printf("The reedb container was now successfully freed \n");
 	return 0;
 }
+
+//
+//int main(void)
+//{
+//	ree_err_t rtn;
+//
+//	struct reedb_c *container;
+//
+//	rtn = rdb_set_passlength(16);
+//	rtn = rdb_set_verbose(true);
+//	rtn = rdb_set_path("/etc/reedb/");
+//
+//	rtn = reedb_init(&container);
+//	if (rtn != BIG_SUCCESS)
+//	{
+//		fputs("An error occurred when initialising Reedb!\n", stderr);
+//	}
+//	else
+//	{
+//		printf("Reedb was initialised successfully!\n");
+//	}
+//
+//	free(container);
+//	printf("The reedb container was now successfully freed \n");
+//	return 0;
+//}
