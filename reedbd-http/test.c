3dd7de78809a25793c07adeeeea50e68212be7ff
Katharina Sabel <sabel.katharina@gmail.com>
2015-09-19 13:44:00 +0200

    Fixing something in the hashmap.h to not throw warnings anymore. Added a little test script to work out how I want to handle data in my structs. Also adding a folder called 'tests' where I moved the Judy array tests. Will go with normal hashmap implementation for now. Performance is secondary to functionality :)

--- reedbd-http/test.c
+++ reedbd-http/test.c
@@ -1,77 +1,59 @@
-// http://www.hp.com/go/judy/          Copyright 2002 Hewlett -Packard Co                       Page 4 of 5
-// Sample program to show how to use Judy as a collision
-// handler within a Hash table.
-//
-//      cc -DHASHSIZE=256 hash.c ..
+
+// Reedb includes
+#include "reedb/utils/hashmap.h"
+
+// System includes
 #include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
-#define JUDYERROR_SAMPLE 1     // use default Judy error handler
-#include <Judy.h>
-// Needed for timing routines
-#include <sys/time.h>
-// Start of timing routines ========================================
-struct timeval TBeg, TEnd;
-#define STARTTm  gettimeofday(&TBeg, NULL)
-#define ENDTm    gettimeofday(&TEnd, NULL)
-#define DeltaUSec \
-	(  ((double)TEnd.tv_sec * 1000000.0 + (double)TEnd.tv_usec) \
-	- ((double)TBeg.tv_sec * 1000000.0 + (double)TBeg.tv_usec) )
+#include <stdbool.h>
+#include <string.h>
 
-// End of timing routines   ========================================
-// Define Hash table size if not in compile line ===================
-// Set HASHSIZE 1  for straight Judy
-#ifndef HASHSIZE
-#define HASHSIZE (1 << 8)     // hash table size 256
-#endif
-// Seed for pseudo-random counter ==================================
-#define INITN          123456          // first Index to store
-static uint32_t                // Placed here for INLINE possibility
-Random(uint32_t Seed)  // produce 2^32 -1 numbers by different counting
+/* Data type enum */
+typedef enum rdb_data_t
 {
-    if ((int32_t)Seed < 0)
-    { 
-    	Seed += Seed; Seed ^= 16611; 
-    } else { 
-    	Seed += Seed;
-    }
+	string, integer, boolean
+} rdb_data_t;
 
-    return(Seed);
-}
-// Hash Table ======================================================
-Pvoid_t JArray[HASHSIZE] = { NULL }; // Declare static hash table
-int main(int argc, char *argv[])
+/* Data struct */
+typedef struct rdb_gendata
 {
-    Word_t Count;
-    Word_t Index;
-    Word_t *PValue;
-    Word_t NumIndexes = 10000;  // default first parameter
-    if (argc > 1) NumIndexes = strtoul(argv[1], NULL, 0);
-    //      Load up the CPU cache for small measurements:
-    for (Count = 0; Count < HASHSIZE; Count++) JArray[Count] = NULL;
-    printf("Begin storing %lu random numbers in a Judy scalable hash array\n",
-    NumIndexes);
-    Index = INITN;
-    STARTTm;
-    for (Count = 0; Count < NumIndexes; Count++)
-    {
-        Index = Random(Index);
-        JLI(PValue, JArray[Index % HASHSIZE], Index/HASHSIZE);
-        // http://www.hp.com/go/judy/          Copyright 2002 Hewlett -Packard Co                       Page 5 of 5
-        *PValue += 1; // bump count of duplicate Indexes
-    }
-    ENDTm;
-    printf("Insertion of %lu indexes took %6.3f microseconds per index\n", NumIndexes, DeltaUSec/NumIndexes);
-    Index = INITN;  // start the same number sequence over
-    STARTTm;
-    for (Count = 0; Count < NumIndexes; Count++)
-    {
-        Index = Random(Index);
-        JLG(PValue, JArray[Index % HASHSIZE], Index/HASHSIZE);
-        if (*PValue != 1)
-        printf("%lu dups of %lun", *PValue - 1, Index);
-    }
-    ENDTm;
-    printf("Retrieval of %lu indexes took %6.3f microseconds per index\n", NumIndexes, DeltaUSec/NumIndexes);
-    return(0);
+	rdb_data_t 	type;
+	size_t			size; // > 1 means it's an array.
+
+	union v
+	{
+		int 			*iptr;
+		char			*cptr;
+		bool 			*bptr;
+	} v;
+
+} rdb_gendata;
+
+/* Main entry point for our binary */
+int main(void)
+{
+	rdb_gendata *data = malloc(sizeof(rdb_gendata));
+	data->type = string;
+	char tmp[] = "This is an awesome passphrase!";
+
+	// Allocate memory for our string dynamically and store in union
+	data->v.cptr = malloc((strlen(tmp) + 1) * sizeof(char));
+	strcpy(data->v.cptr, tmp);
+
+	// Overwrite our temporary stack array for security reasons.
+	memset(&tmp, 0, sizeof(tmp));
+
+	//TODO: Stuff with our data. It's now in a struct. 
+	// 			Decide what to do with it, encrypt it, then store
+
+
+	// Free our data from the union, depending on what type it was.
+	if(data->type == string) free(data->v.cptr);
+	else if(data->type == boolean) free(data->v.bptr);
+	else if(data->type == integer) free(data->v.iptr);
+
+	// Finally free the data struct itself
+	free(data);
+
+	// And return the all clear.
+	return 0;
 }
