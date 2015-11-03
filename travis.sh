28ba8981fd20462c308b68bb2c5060eb2da9d733
SpaceKookie <sabel.katharina@gmail.com>
2015-11-03 11:31:45 +0100

    fixing my travis build script :) Finally

--- travis.sh
+++ travis.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-build_cmake() {
-  CFLAGS="-fsanitize=undefined"
-  mkdir $PWD/_build && cd $PWD/_build
-  cmake .. -DCMAKE_INSTALL_PREFIX=$PWD/../_install
-  cmake --build . --target install
-  ctest -V .
-}
-
 set -e  # exit on error.
 set -x  # echo commands.
 
-eval "build_cmake"
+CFLAGS="-fsanitize=undefined"
+mkdir $PWD/_build && cd $PWD/_build
+cmake ..
+cmake --build .
+
+# Clean up
+cd $PWD/.. && rm -rf $PWD/_build
