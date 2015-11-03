97d76a065a29d07255537dc59b4b96d5b97c2515
SpaceKookie <sabel.katharina@gmail.com>
2015-11-03 11:52:24 +0100

    BLAAA

--- travis.sh
+++ travis.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-#
-# This is the reedb build script that compiles
-# Reedb for travis on Linux systems.
+# ############################################## #
+#                                                #
+#  This is the reedb build script that compiles  #
+#  Reedb for travis on Linux systems.            #
+#                                                #
+# ############################################## #
 
 set -e  # exit on error.
 set -x  # echo commands.
 
 # Preparation
 if [ ! -f ./cmake.tar.gz ]; then
-	wget https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz -O cmake.tar.gz
+	wget --no-check-certificate https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz -O cmake.tar.gz
 	mkdir my_cmake && tar xf cmake.tar.gz -C my_cmake
 	alias cmake=$PWD/my_cmake/bin/cmake
 fi
