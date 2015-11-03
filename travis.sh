94cb0d99d3cda96af659cd4ea2b5d47bf8b66f3f
SpaceKookie <sabel.katharina@gmail.com>
2015-11-03 11:49:44 +0100

    Changing back cmake version and getting my own cmake

--- travis.sh
+++ travis.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
+#
+# This is the reedb build script that compiles
+# Reedb for travis on Linux systems.
+
 set -e  # exit on error.
 set -x  # echo commands.
 
+# Preparation
+if [ ! -f ./cmake.tar.gz ]; then
+	wget https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz -O cmake.tar.gz
+	mkdir my_cmake && tar xf cmake.tar.gz -C my_cmake
+	alias cmake=$PWD/my_cmake/bin/cmake
+fi
+
+# Actual build
 CFLAGS="-fsanitize=undefined"
 mkdir $PWD/_build && cd $PWD/_build
 cmake ..
@@ -10,7 +22,3 @@ cmake --build .
 
 # Clean up
 cd $PWD/.. && rm -rf $PWD/_build
-
-echo '  ######################'
-echo 'Build passed successfully!'
-echo '  ######################'
