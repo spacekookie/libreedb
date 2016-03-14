b6b3275da1c9bc2696af7b3c1b272e3a28d7665c
SpaceKookie <sabel.katharina@gmail.com>
2016-03-14 18:15:03 +0100

    Updating the UUID utility

--- reedb/include/reedb/utils/uuid.h
+++ reedb/include/reedb/utils/uuid.h
@@ -2,39 +2,21 @@
 #define __UUID_H__
 
 typedef enum uuid_type {
-  ONE, TWO, THREE
+    ONE, TWO, THREE
 } uuid_type;
 
 typedef struct rdb_uuid {
-  char id[32];
-  uuid_type type;
+    char id[32];
+    uuid_type type;
 } rdb_uuid;
 
 class uuid_helper {
 
-private:
-  uuid_type type;
-  char id[32];
-  
 public:
-  rdb_uuid *generate();
-  rdb_uuid *generate(uuid_type type);
-  
-};
 
-// typedef enum uuid_type {
-//   ONE, TWO, THREE
-// } uuid_type;
-// 
-// typedef struct rdb_uuid {
-//   char id[32];
-//   uuid_type type;
-// } rdb_uuid;
-// 
-// /** Generates a standard UUID */
-// rdb_uuid rdb_generate_uuid();
-// 
-// /* */
-// rdb_uuid *rdb_generate_uuid_with_type(uuid_type type);
+    /* C like function you should use for UUIDs */
+    int rdb_uuid_generate(rdb_uuid *(*uuid), uuid_type type);
+
+};
 
 #endif /* __UUID_H__ */
--- reedb/src/utils/uuid.cpp
+++ reedb/src/utils/uuid.cpp
@@ -1,11 +1,35 @@
 #include "reedb/utils/uuid.h"
+#include "crypto/rcry_utils.h"
 
-rdb_uuid *uuid_helper::generate(uuid_type type)
-{
+#include <iostream>
+#include <string.h>
 
-}
+using namespace std;
 
-rdb_uuid *uuid_helper::generate()
-{
+int uuid_helper::rdb_uuid_generate(rdb_uuid **uuid, uuid_type type) {
+    (*uuid) = (struct rdb_uuid *) malloc(sizeof(struct rdb_uuid));
+    (*uuid)->type = THREE;
 
+    char *A = rcry_utils::generate_random(4, true);
+    char *B = rcry_utils::generate_random(3, true);
+    char *C = rcry_utils::generate_random(3, true);
+    char *D = rcry_utils::generate_random(2, true);
+    char *E = rcry_utils::generate_random(2, true);
+
+    strcpy((*uuid)->id, A);
+    strcat((*uuid)->id, "-");
+
+    strcat((*uuid)->id, B);
+    strcat((*uuid)->id, "-");
+
+    strcat((*uuid)->id, C);
+    strcat((*uuid)->id, "-");
+
+    strcat((*uuid)->id, D);
+    strcat((*uuid)->id, "-");
+
+    strcat((*uuid)->id, E);
+
+    cout << "UUID: " << (*uuid)->id << endl;
+    return 0;
 }
