f4da715ce922703b4d4c44996aad04eb965c39b1
SpaceKookie <sabel.katharina@gmail.com>
2016-03-20 02:57:35 +0100

    Updating the datafile code to now properly use the name and path variables.

--- reedb/src/datafile.cpp
+++ reedb/src/datafile.cpp
@@ -1,45 +1,55 @@
-
 /* Internal Reedb requirements */
 #include "datafile.h"
 
 /* Some system requirements for data storage and file access*/
 #include <iostream>
 #include <vector>
-#include <string>
 #include <map>
+
+#include <string.h> // C string support
+#include <string> // C++ strings
+
+#include <crypto/rcry_utils.h>
 
 using namespace std;
 
-datafile::datafile(string name, file_path *parent)
-{
-  /* Asign data to fields */
-  this->name = name;
-  this->path = *parent->path + "/" + name;
-  this->version = 1;
+datafile::datafile(string name, string parent) {
 
-  std::cout << "Just created a new datafile" << endl;
-  
-  /* Then populate them */
-  this->populate();
-  
-  /* Set cache mode to hotdrop so we can use the file immediately */
-  this->cache(HOTDROP);
-  this->write();
-  
-  std::cout << "File dump complete. Ready to cache it from disk..." << endl;
+    /* Asign data to fields */
+    this->name = name;
+
+    char *buffer = rcry_utils::md_sha256(name.c_str(), true);
+    for (int i = 0; buffer[i] != '\0'; i++) {
+        buffer[i] = tolower(buffer[i]);
+    }
+
+    this->path = parent + buffer + ".rdf"; // Reedb datafile
+    this->version = 1;
+
+    free(buffer);
+
+    cout << "File path: " << this->path << endl;
+
+    std::cout << "Just created a new datafile" << endl;
+
+    /* Then populate them */
+    this->populate();
+
+    /* Set cache mode to hotdrop so we can use the file immediately */
+    this->cache(HOTDROP);
+    this->write();
+
+    std::cout << "File dump complete. Ready to cache it from disk..." << endl;
 }
 
-void datafile::populate()
-{  
-  this->data = new vector<map<string, void*>*>;
+void datafile::populate() {
+    this->data = new vector<map<string, void *> *>;
 }
 
-void datafile::write()
-{
+void datafile::write() {
 
 }
 
-void datafile::cache(const cache_mode mode)
-{
-  
+void datafile::cache(const cache_mode mode) {
+
 }
--- reedb/src/datafile.h
+++ reedb/src/datafile.h
@@ -8,19 +8,19 @@
 using namespace std;
 
 typedef enum cache_mode {
-  /* Drop from cache after every action */
-  HOTDROP, 
-  
-  /* Keep in cache for limited time (defined in config) */
-  TIMEOUT, 
-  
-  /* Keep in cache until daemon shuts down */
-  ENDLESS
+    /* Drop from cache after every action */
+            HOTDROP,
+
+    /* Keep in cache for limited time (defined in config) */
+            TIMEOUT,
+
+    /* Keep in cache until daemon shuts down */
+            ENDLESS
 } cache_mode;
 
 typedef struct file_path {
-  string *path;
-  int opMode;
+    string *path;
+    int opMode;
 } file_path;
 
 /** 
@@ -29,93 +29,92 @@ typedef struct file_path {
  * quick access via RQL
  */
 typedef struct datafile_h {
-  string *name;
-  string *tags;
-  string category;
-  
-  unsigned int size;
-  unsigned int version;
-  bool locked;
-} datafile_h ;
+    string *name;
+    string *tags;
+    string category;
 
-class datafile
-{
+    unsigned int size;
+    unsigned int version;
+    bool locked;
+} datafile_h;
+
+class datafile {
 public:
-  
-  /** 
-   * Default constructor that creates a datfile. It goes through
-   * the entire file creation process and optionally also creates
-   * new keys for zones it should be long to.
-   * 
-   * @param name Name of the file (in cleartext)
-   * @param *parent Struct that contain parent path + permissions
-   */
-  datafile(string name, file_path *parent);
-  
-  /** 
-   * Use this constructor to initiate a file that has previously 
-   * existed.
-   * 
-   * @param name Name of the file (in cleartext)
-   * @param oldFile Contents on the old file as binary buffer
-   */
-  datafile(string name, string oldFile);
-  
-  /**
-   * 
-   */
-  void insertData(string data);
-  
-  /**
-   * 
-   */
-  // void readFile(map *(*data));
-  
-  /**
-   * @param mode: The mode that should be used for caching. See @"CacheMode"
-   */
-  void cache(const cache_mode mode);
-  
-  /**
-   * Closes the file and uncaches it.
-   */
-  void close();
-  
-  /**
-   * Deletes this datafile, scrubs it from disk and removes the encryption
-   * keys from the keystore.
-   */
-  
+
+    /**
+     * Default constructor that creates a datfile. It goes through
+     * the entire file creation process and optionally also creates
+     * new keys for zones it should be long to.
+     *
+     * @param name Name of the file (in cleartext)
+     * @param *parent Struct that contain parent path + permissions
+     */
+    datafile(string name, string parent);
+
+    /**
+     * Use this constructor to initiate a file that has previously
+     * existed.
+     *
+     * @param name Name of the file (in cleartext)
+     * @param oldFile Contents on the old file as binary buffer
+     */
+    datafile(string name, map<string, string> *oldFile);
+
+    /**
+     *
+     */
+    void insertData(string data);
+
+    /**
+     *
+     */
+    // void readFile(map *(*data));
+
+    /**
+     * @param mode: The mode that should be used for caching. See @"CacheMode"
+     */
+    void cache(const cache_mode mode);
+
+    /**
+     * Closes the file and uncaches it.
+     */
+    void close();
+
+    /**
+     * Deletes this datafile, scrubs it from disk and removes the encryption
+     * keys from the keystore.
+     */
+
 private:
-  
-  /* Some global metadata */
-  string name;
-  string path;
-  int version;
-  bool locked;
-  
-  /* Database metadata for queries */
-  string *category;
-  string *tags;
-  
-  /* Vector of data maps. Each version creates a new map */
-  std::vector<std::map<string, void*>*> *data;
-  
-  /* @return If file is locked */
-  bool isLocked();
-  
-  /** 
-   * Populate a new file with basic data so there are no nullpointers
-   * that can get thrown.
-   */
-  void populate();
-  
-  /** 
-   * Fills the protocol buffer, pipes the bytestream through the 
-   * crypto engine and then dumps that to disk as base64 encoded
-   */
-  void write();
-  
+
+    /* Some global metadata */
+    string name;
+    string path;
+    int version;
+    bool locked;
+
+    /* Database metadata for queries */
+    string *category;
+    string *tags;
+
+    /* Vector of data maps. Each version creates a new map */
+    std::vector<std::map<string, void *> *> *data;
+
+    /* @return If file is locked */
+    bool isLocked();
+
+    /**
+     * Populate a new file with basic data so there are no nullpointers
+     * that can get thrown.
+     */
+    void populate();
+
+    /**
+     * Fills the protocol buffer, pipes the bytestream through the
+     * crypto engine and then dumps that to disk as base64 encoded
+     */
+    void write();
+
 };
 
 #endif // RDBDATAFILE_H
