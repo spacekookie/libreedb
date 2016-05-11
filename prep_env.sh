1d5eb364d8db6dd8f4c5c729e394e7942cd85f7f
SpaceKookie <sabel.katharina@gmail.com>
2016-05-11 11:33:38 +0200

    Adding missing dependency for travis build

--- prep_env.sh
+++ prep_env.sh
@@ -6,9 +6,15 @@ echo "##################### PREPARING ENVIRONMENT BECAUSE FUCK TRAVIS! #########
 # - libgcrypt-dev   # C crypto - secure RAM
 # - libcryptopp-dev # C++ crypto - ciphers & keygen
 
+# Installs the required version of libgpgerror for libgcrypt
+wget --no-check-certificate https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2 -O gpgerror.tar.bz2
+mkdir _my_gpgerror && tar xf gpgerror.tar.bz2 -C _my_gpgerror --strip 1
+cd _my_gpgerror && ./configure && make && sudo make install
+
+
 # Installs the required version of libgcrypt for the build
-wget --no-check-certificate https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2 -O libgcrypt.tar.gz
-mkdir _my_gcrypt && tar xf libgcrypt.tar.gz -C _my_gcrypt --strip 1
+wget --no-check-certificate https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2 -O libgcrypt.tar.bz2
+mkdir _my_gcrypt && tar xf libgcrypt.tar.bz2 -C _my_gcrypt --strip 1
 cd _my_gcrypt && ./configure && make && sudo make install
 
 # Installs the required version of libcrypto++ for the build
