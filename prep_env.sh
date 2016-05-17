a7ce764a249a51fcaddf87cb2bdad4dec0c0d5d6
SpaceKookie <sabel.katharina@gmail.com>
2016-05-17 14:18:31 +0200

    Tweaking build tools

--- prep_env.sh
+++ prep_env.sh
@@ -17,15 +17,17 @@ wget --no-check-certificate https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7
 mkdir _my_gcrypt && tar xf libgcrypt.tar.bz2 -C _my_gcrypt --strip 1
 cd _my_gcrypt && ./configure && make && sudo make install
 
+
 # Installs the required version of libcrypto++ for the build
 wget --no-check-certificate https://www.cryptopp.com/cryptopp563.zip -O cryptopp.zip
 mkdir ./_my_cryptopp && unzip cryptopp.zip -d ./_my_cryptopp
 cd ./_my_cryptopp && make && sudo make install
 
-# General dependencies
-sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" -y
-sudo apt-get update -qq
-sudo apt-get install -qq libboost-test-dev g++ 
+
+# Download and compile boost
+wget --no-check-certificate http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download -O boost.tar.bz2
+mkdir _my_boost && tar xf boost.tar.bz2 -C _my_boost --strip 1
+cd ./_my_boost && sudo ./bootstrap.sh --prefix=/usr/local && sudo ./b2 install
 
 # Download and install libboost-filesystem
 # wget --no-check-certificate http://ftp.de.debian.org/debian/pool/main/b/boost-defaults/libboost-filesystem-dev_1.58.0.1_amd64.deb -O boost-filesystem-dev.deb
